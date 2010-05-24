1e29f2eeb8d976f9e867c83a9a35ec740abfc1a2
Pinhook <contact@pinhooklabs.com>
2010-05-24 18:47:24 -0500

    attributes can be downloaded

--- attribute.js
+++ attribute.js
@@ -15,10 +15,10 @@ DocumentJS.Pair.extend('DocumentJS.Attribute',
         this.add(DocumentJS.Directive.Return, DocumentJS.Directive.Param, DocumentJS.Directive.CodeStart, 
         DocumentJS.Directive.CodeEnd,DocumentJS.Directive.Plugin, DocumentJS.Directive.Hide, 
         DocumentJS.Directive.Tag, DocumentJS.Directive.iFrame, DocumentJS.Directive.Demo,
-		DocumentJS.Directive.Parent, DocumentJS.Directive.Type);
+		DocumentJS.Directive.Parent, DocumentJS.Directive.Type, DocumentJS.Directive.Download);
         this._super();
 				
-		this.serialize('plugin',['full_name','name'], 'html', 'shortName', 'ret','params', ['real_comment','comment'])
+		this.serialize('plugin',['full_name','name'], 'html', 'shortName', 'ret','params', ['real_comment','comment'],'download')
 	},
 
      /**
