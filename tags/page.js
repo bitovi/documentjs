623df2d4af856f31b925d138c9b56fe45c45b526
Brian Moschel <brian.moschel@gmail.com>
2010-10-09 03:04:53 -0500

    removing need to use * on every line

--- tags/page.js
+++ tags/page.js
@@ -1,6 +1,6 @@
 DocumentJS.Tags.page = {
     add: function(line){
-        var m = line.match(/^@\w+\s+([^\s]+)\s+(.+)/)
+        var m = line.match(/^\s*@\w+\s+([^\s]+)\s+(.+)/)
         if(m){
             this.name = m[1];
             this.title = m[2] || this.name;
