e005545bb76aab72f4a6d911c0cc5a2c7fb7ce43
Josh Dean <imjoshdean@me.com>
2013-11-04 10:52:46 -0600

    Add JSFiddle tab/link to @demo tabs.

--- site/default/static/demo_frame.js
+++ site/default/static/demo_frame.js
@@ -5,7 +5,7 @@ steal("can/control","./demo_frame.mustache","jquery","can/observe","./prettify.j
 return can.Control({
 	init: function() {
 		// Render out the demo container.
-		this.element.html(demoFrameMustache( {demoSrc: '../' + this.element.data('demoSrc')}));
+		this.element.html(demoFrameMustache( {demoSrc: '../' + this.element.data('demoSrc'), jsfiddle: !!window.Bitovi.JSFIDDLE }));
 
 		// Start with the demo tab showing.
 		this.showTab('demo');
@@ -16,6 +16,9 @@ return can.Control({
 		iFrame.load(function() {
 			var demoEl = this.contentDocument.getElementById('demo-html'),
 				sourceEl = this.contentDocument.getElementById('demo-source')
+
+			self.demoBody = $(this.contentDocument.body).clone();
+			self.demoHead = $(this.contentDocument.head).clone();
 
 			var html = demoEl ? demoEl.innerHTML : this.contentWindow.DEMO_HTML;
 			
@@ -62,7 +65,23 @@ return can.Control({
 		});
 	},
 	'.tab click': function(el, ev) {
-		this.showTab(el.data('tab'));
+		var tab = el.data('tab');
+		if(tab === 'jsfiddle') {
+			if(!this.fiddleData) {
+				this.fiddleData = this.scrapeForFiddle(this.demoHead, this.demoBody);
+
+			  this.element.find('input[name=resources]').val(this.fiddleData.resources.join(','));
+			  this.element.find('input[name=html]').val(this.fiddleData.html.join(' '));
+			  this.element.find('input[name=css]').val(this.fiddleData.styles.join(' '));
+			  this.element.find('input[name=js]').val(this.fiddleData.scripts.join(' '));	
+			}
+
+		  this.element.find('form').submit();
+		}
+		else {
+			this.showTab(el.data('tab'));	
+		}
+		
 	},
 	showTab: function(tabName) {
 		$('.tab', this.element).removeClass('active');
@@ -72,6 +91,46 @@ return can.Control({
 	},
 	prettify: function(unescaped) {
 		return prettyPrintOne(unescaped.replace(/</g, '&lt;'));
+	},
+	scrapeForFiddle: function(head, body) {
+	  var resources = [];
+	    scripts = [],
+	    styles = [],
+	    html = [],
+		stripScriptStyle = function(elem) {
+	    // Grab styles
+	    elem.find('style').each(function(i, style) {
+	    	styles.push(style.textContent);
+	    }).remove();
+
+	    // Grab link resources
+	    elem.find('link').each(function(i, link) {
+	    	resources.push(link.href);
+	    }).remove();
+
+	    // Grab script resources
+	    elem.find('script[src]').each(function(i, script) {
+	    	resources.push(script.src);
+	    }).remove();
+
+	    // Grab inline JavaScript
+	    elem.find("script:not([src]):not([type$=mustache]:not([type$=ejs]))").each(function(i, script) {
+	    	scripts.push(script.text);
+	    }).remove();
+
+		};
+
+		stripScriptStyle(head);
+		stripScriptStyle(body);
+
+    html.push(body.html());
+
+    return {
+    	resources: resources,
+    	scripts: scripts,
+    	styles: styles,
+    	html: html
+    };
 	}
 });
 
