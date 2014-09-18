5d18247b9ce38208844119aa3b7514cf71fa4770
Justin Meyer <justinbmeyer@gmail.com>
2014-09-18 18:18:48 -0500

    dont show types if signatures

--- examples/multi-module-project/util/add.js
+++ examples/multi-module-project/util/add.js
@@ -1,6 +1,16 @@
 /**
- * @function add
+ * @module {function} add
  * @parent multi-module-project
+ * 
+ * Adds two numbers together.
+ * 
+ * @signature `sum(first, second)`
+ * 
+ * @param {Number} first
+ * 
+ * @body
+ * 
+ * ## Use
  */
 
 module.exports = function(){}
